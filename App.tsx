701045a058c95a47f012a79faf1ce71f952ce3e2
mgckaled <marcelgckaled@gmail.com>
2022-12-09 15:19:19 -0300

    style: add style guide (main theme)

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar'
-import { Groups } from './src/screens/Groups'
+import { ThemeProvider } from 'styled-components/native'
+
+import theme from '@theme/theme'
+import { Groups } from '@screens/Groups'
 
 export default function App() {
 	return (
-		<>
+		<ThemeProvider theme={theme}>
 			<Groups />
 			<StatusBar style="auto" />
-		</>
+		</ThemeProvider>
 	)
 }
