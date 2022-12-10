a2567c389fcb1230335f64fb084d75f10bd8f739
mgckaled <marcelgckaled@gmail.com>
2022-12-10 14:04:39 -0300

    feat: create ListEmpty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { FlatList } from 'react-native'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { GroupCard } from '@components/GroupCard'
+import { ListEmpty } from '@components/ListEmpty'
 
 import { Container } from './styles'
 
@@ -24,6 +25,10 @@ export function Groups() {
 				data={groups}
 				keyExtractor={item => item}
 				renderItem={({ item }) => <GroupCard title={item} />}
+				contentContainerStyle={groups.length === 0 && { flex: 1 }}
+				ListEmptyComponent={() => (
+					<ListEmpty message="Que tal cadastrar a primeira turma?" />
+				)}
 			/>
 		</Container>
 	)
