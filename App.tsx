3f23b9955f6285add316d1fc00e7c4d40e0e7b28
mgckaled <marcelgckaled@gmail.com>
2022-12-12 12:07:41 -0300

    refactor: use CSS helper to refactor style code

--- App.tsx
+++ App.tsx
@@ -22,7 +22,7 @@ export default function App() {
 					backgroundColor="transparent"
 					translucent
 				/>
-				{fontsLoaded ? <NewGroup /> : <Loading />}
+				{fontsLoaded ? <Groups /> : <Loading />}
 			</>
 		</ThemeProvider>
 	)
