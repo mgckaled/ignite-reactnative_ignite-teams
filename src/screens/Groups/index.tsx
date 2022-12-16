2f716135720bf4845cabda1abfc2b37377855ace
mgckaled <marcelgckaled@gmail.com>
2022-12-16 14:55:04 -0300

    feat: implement back navigations routes

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -16,7 +16,7 @@ export function Groups() {
 	const navigation = useNavigation()
 
 	function handleNewGroup() {
-		navigation.navigate('players', { group: 'Galera do Ignite' })
+		navigation.navigate('new')
 	}
 
 	return (
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from '@react-navigation/native'
+
 import { Button } from '@components/Button'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
@@ -6,6 +8,12 @@ import { Input } from '@components/Input'
 import { Container, Content, Icon } from './styles'
 
 export function NewGroup() {
+	const navigation = useNavigation()
+
+	function handleNew() {
+		navigation.navigate('players', { group: 'Rocket' })
+	}
+
 	return (
 		<Container>
 			<Header showBackButton />
@@ -16,7 +24,7 @@ export function NewGroup() {
 					subtitle="crie a turma para adicionar as pessoas"
 				/>
 				<Input placeholder="Nome da turma" />
-				<Button title="Criar" style={{ marginTop: 20 }} />
+				<Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
 			</Content>
 		</Container>
 	)
